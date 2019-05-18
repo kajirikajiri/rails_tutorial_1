53f1f69f1344484b6d2cc4dc71be8c1e8eab379f
kazukinakamuraKN <kajirikajiri@gmail.com>
2019-05-18 12:26:28 +0000

    save

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -14,7 +14,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "#{@base_title}", "hlajsdfkl;"
+    assert_select "#{@base_title}"
   end
 
   test "should get help" do
