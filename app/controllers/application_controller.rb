28d5ababcc7866062c33f8d01f36cabd49adcc63
kazukinakamuraKN <kajirikajiri@gmail.com>
2019-05-18 05:42:47 +0000

    save

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception # yosonosaitonokinouwoyobidasukougeki wo husegu
+
+  def hello
+    render html: "hello,world"
+  end
 end
